8423e1863e7376c103de357bf2d244f7347644be
dushyant1295 <dushyant1295@gmail.com>
2020-11-11 20:07:44 +0530

    optimized

---
+++ README.md
@@ -0,0 +1,12 @@
+# dpk.js
+#### 🤍 Collection of High Performance Super smooth functions that help you to create awwward winning websites. 
+
+
+### Dependencies
+
+| Name             | Description                                                        |
+| ---------------- | ------------------------------------------------------------------ |
+| [💚 Greensock]  | Gsap is a JavaScript library for building high-performance animations that work in every major browser. |
+
+[💚 Greensock]:https://greensock.com/
+                                        
--- js/Module/dpkCursor.js
+++ js/Module/dpkCursor.js
@@ -3,10 +3,14 @@ import { isMobile } from "./Helper";
 const dpkCursor = document.querySelector(".dpkCursor");
 
 function initCursor() {
-  if (dpkCursor) {
+  if (!isMobile() && dpkCursor) {
     gsap.set(dpkCursor, { xPercent: -50, yPercent: -50 });
+
     let dpkCursorPos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
     let dpkCursorMouse = { x: dpkCursorPos.x, y: dpkCursorPos.y };
+
+    let dpkCursorXSet = gsap.quickSetter(dpkCursor, "x", "px");
+    let dpkCursorYSet = gsap.quickSetter(dpkCursor, "y", "px");
 
     window.addEventListener("mousemove", (e) => {
       gsap.to(dpkCursorMouse, {
@@ -18,20 +22,17 @@ function initCursor() {
       });
     });
 
-    let dpkCursorXSet = gsap.quickSetter(dpkCursor, "x", "px");
-    let dpkCursorySet = gsap.quickSetter(dpkCursor, "y", "px");
-
     gsap.ticker.add(() => {
       dpkCursorPos.x += dpkCursorMouse.x - dpkCursorPos.x;
       dpkCursorPos.y += dpkCursorMouse.y - dpkCursorPos.y;
       dpkCursorXSet(dpkCursorPos.x);
-      dpkCursorySet(dpkCursorPos.y);
+      dpkCursorYSet(dpkCursorPos.y);
     });
   }
 }
 
 function resetCursor() {
-  dpkCursor.querySelector(".hover-circle").innerHTML = "";
+  dpkCursor.innerHTML = "";
   dpkCursor.classList.remove("hover-active");
   gsap.set(dpkCursor, {
     background: "",
@@ -47,42 +48,33 @@ function cursorEffects() {
   if (!isMobile() && dataHover) {
     dataHover.forEach(function (target) {
       target.addEventListener("mouseenter", function () {
-        let dataHoverText = target.getAttribute("data-hoverMsg");
-        let emogy = target.getAttribute("data-emogy");
-        let datafillbg = target.getAttribute("data-fillBg");
+        let dataHoverText = target.getAttribute("data-hover-text");
+        let emogy = target.getAttribute("data-hover-emogy");
+        let datafillbg = target.getAttribute("data-hover-bg");
 
         dpkCursor.classList.add("hover-active");
 
-        if (dataHoverText) {
-          dpkCursor.querySelector(".hover-circle").innerHTML = dataHoverText;
-        }
+        if (dataHoverText) dpkCursor.innerHTML = dataHoverText;
 
         if (emogy) {
-          dpkCursor.querySelector(".hover-circle").innerHTML = emogy;
+          dpkCursor.innerHTML = emogy;
           gsap.set(dpkCursor, { mixBlendMode: "normal", borderColor: "gray" });
         }
 
         if (datafillbg) {
-          const mixBlendMode =
-            datafillbg === "mixBlend" ? "difference" : "normal";
+          const blendMode = datafillbg === "mixBlend" ? "difference" : "normal";
           const fillColor = datafillbg === "mixBlend" ? "white" : datafillbg;
           gsap.set(dpkCursor, {
             background: fillColor,
             borderWidth: 0,
-            mixBlendMode: mixBlendMode,
+            mixBlendMode: blendMode,
           });
         }
       });
 
-      target.addEventListener("mouseleave", function () {
-        resetCursor();
-      });
-
-      target.addEventListener("click", function () {
-        resetCursor();
-      });
+      target.addEventListener("mouseleave", resetCursor);
+      target.addEventListener("click", resetCursor);
     });
   }
 }
-
 export { initCursor, resetCursor, cursorEffects };
