ff42aa35e44d59332b02b2fb3fc50bc705b9182f
dushyant1295 <dushyant1295@gmail.com>
2021-06-10 19:54:14 +0530

    dpk.min.js

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,3 @@ import  { initCursor, resetCursor, cursorEffects }  from  "./Module/dpkCursor";
 initCursor();
 cursorEffects();
 
-
-
-
-// create a function dpkInit(); where user can initialize all function of dpk.js
